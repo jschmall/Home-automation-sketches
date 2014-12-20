f511cf082753fb3c0d2b272323b91a3c4143e274
jschmall <jschmall@gmail.com>
2014-12-19 17:01:22 -0800

    Removed all time related code

--- room_presence_2_0/room_presence_2_0.ino
+++ room_presence_2_0/room_presence_2_0.ino
@@ -1,15 +1,15 @@
 // Libraries included
-#include "DHT.h"            // Temp/Humidity Sensor Library
-#include <UIPEthernet.h>    
-// ENC SO -> Arduino pin 12
-// ENC SI -> Arduino pin 11
-// ENC SCK -> Arduino pin 13
-// ENC CS -> Arduino pin 10
-// ENC VCC -> Arduino 3V3 pin
-// ENC GND -> Arduino Gnd pin
-
-#include <PubSubClient.h>   // MQTT Library
-#include <Time.h>           // Time Library
+#include "DHT.h"              // Temp/Humidity Sensor Library
+//#include <UIPEthernet.h>    // Breakout board for ethernet
+  // ENC SO -> Arduino pin 12
+  // ENC SI -> Arduino pin 11
+  // ENC SCK -> Arduino pin 13
+  // ENC CS -> Arduino pin 10
+  // ENC VCC -> Arduino 3V3 pin
+  // ENC GND -> Arduino Gnd pin
+#include <Ethernet.h>
+#include <SPI.h>
+#include <PubSubClient.h>     // MQTT Library
 
 // Definitions
 #define DHTPIN 2            // DHT Data Pin
@@ -25,15 +25,9 @@ boolean takeLowTime;
 long previousMillis = 0;              // Defining previousMillis for millis() loop
 long previousMillis2 = 0;             // Defining previousMillis2 for millis() loop
 long interval = 3000;                 // Time to wait before next DHT read
-long interval2 = 500;
+long interval2 = 1000;
 int pirPin = 3;                       // PIR Data Pin
 int LDR_Pin = A5;                     // Photocell Analog Pin
-unsigned int localPort = 8888;        // local port to listen for UDP packets
-const int timeZone = -8;              // Central European Time
-//const int timeZone = -5;            // Eastern Standard Time (USA)
-//const int timeZone = -4;            // Eastern Daylight Time (USA)
-//const int timeZone = -8;            // Pacific Standard Time (USA)
-//const int timeZone = -7;            // Pacific Daylight Time (USA)
 
 // Variables
 byte mac[]    = { 0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };      // MAC address for Ethernet Shield
@@ -41,7 +35,6 @@ byte server[] = { 10, 0, 1, 100 };                           // Static IP for MQ
 byte ip[]     = { 10, 0, 1, 75 };                            // Static IP for Ethernet Shield. Change as needed.
 
 DHT dht(DHTPIN, DHTTYPE);                                    // Define DHT values
-EthernetUDP Udp;                                             //
 
 IPAddress timeServer(10, 0, 1, 1);                           // Assigns the router IP as the Time Server
 
@@ -59,8 +52,6 @@ void setup()
   Ethernet.begin(mac, ip);             // Initialize Ethernet connection using DHCP
   dht.begin();                         // Initialize DHT sensor
   client.connect("bedroom1Client");    // Connect to MQTT broker as "bedroom1Client"
-  Udp.begin(localPort);                // Initialize UDP session
-  setSyncProvider(getNtpTime);         //
   Serial.begin(9600);
 }
 
@@ -70,7 +61,6 @@ void loop()
 {
   atmosphere();        // Run the atmosphere function to get the condition of the room
   presence();          // Run the presence funtion to get status of occupancy
-  delay(500);
 }
 
 // Callback Function for PubSubClient
@@ -93,7 +83,7 @@ void atmosphere()
     dtostrf(h, 3, 1, stringH);                                // Convert floating humidity value to a string with 3 valuse, 2 before the decimal and 1 after
     client.publish("inside/bedroom1/temp", stringTF);         // Publish the temp string to the MQTT broker under "inside/bedroom1/temp"
     client.publish("inside/bedroom1/humidity", stringH);      // Publish the humidity string to the MQTT broker under "inside/bedroom1/humidity"
-  }
+}
 }
 
 // Presence Function
@@ -114,6 +104,8 @@ void presence()
   digitalWrite(trigPin, LOW);                                   // Write Trigger Pin low
   duration = pulseIn(echoPin, HIGH);                            // Define duration variable
   distance = (duration/2) / 2.9;                                // Define distance variable and calculate distance in mm
+  Serial.print("Sonar - ");
+  Serial.println(distance);
   if (distance < 1100) {                                        // Check distance, if less than given value
     client.publish("inside/bedroom1/sonar", "Occupied");        // publish to "inside/bedroom1/sonar" as "Occupied"
   }
@@ -122,7 +114,7 @@ void presence()
   }
   
   // Photocell/Time Detection Module
-
+  Serial.print("Light - ");
   Serial.println(analogRead(LDR_Pin));
 
   // PIR Module
@@ -146,54 +138,4 @@ void presence()
    }
 }
   }
-}
-
-
-//***** ------------------ NEED TO ORGANIZE THIS ------------------ ******//
-
-const int NTP_PACKET_SIZE = 48; // NTP time is in the first 48 bytes of message
-byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
-
-time_t getNtpTime()
-{
-  while (Udp.parsePacket() > 0) ; // discard any previously received packets
-  sendNTPpacket(timeServer);
-  uint32_t beginWait = millis();
-  while (millis() - beginWait < 1500) {
-    int size = Udp.parsePacket();
-    if (size >= NTP_PACKET_SIZE) {
-      Udp.read(packetBuffer, NTP_PACKET_SIZE);  // read packet into the buffer
-      unsigned long secsSince1900;
-      // convert four bytes starting at location 40 to a long integer
-      secsSince1900 =  (unsigned long)packetBuffer[40] << 24;
-      secsSince1900 |= (unsigned long)packetBuffer[41] << 16;
-      secsSince1900 |= (unsigned long)packetBuffer[42] << 8;
-      secsSince1900 |= (unsigned long)packetBuffer[43];
-      return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
-    }
-  }
-  return 0; // return 0 if unable to get the time
-}
-
-// send an NTP request to the time server at the given address
-void sendNTPpacket(IPAddress &address)
-{
-  // set all bytes in the buffer to 0
-  memset(packetBuffer, 0, NTP_PACKET_SIZE);
-  // Initialize values needed to form NTP request
-  // (see URL above for details on the packets)
-  packetBuffer[0] = 0b11100011;   // LI, Version, Mode
-  packetBuffer[1] = 0;     // Stratum, or type of clock
-  packetBuffer[2] = 6;     // Polling Interval
-  packetBuffer[3] = 0xEC;  // Peer Clock Precision
-  // 8 bytes of zero for Root Delay & Root Dispersion
-  packetBuffer[12]  = 49;
-  packetBuffer[13]  = 0x4E;
-  packetBuffer[14]  = 49;
-  packetBuffer[15]  = 52;
-  // all NTP fields have been given values, now
-  // you can send a packet requesting a timestamp:                 
-  Udp.beginPacket(address, 123); //NTP requests are to port 123
-  Udp.write(packetBuffer, NTP_PACKET_SIZE);
-  Udp.endPacket();
 }
